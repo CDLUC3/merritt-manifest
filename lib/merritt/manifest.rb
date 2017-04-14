5e7836225fb07c21830bc6a294c0b337db522f32
David Moles <david.moles@ucop.edu>
2017-04-14 15:29:52 -0700

    Improve docs

--- lib/merritt/manifest.rb
+++ lib/merritt/manifest.rb
@@ -4,8 +4,16 @@ module Merritt
   # A Merritt manifest file
   class Manifest
 
+    # Base for all recognized profile URIs
+    PROFILE_BASE_URI = 'http://uc3.cdlib.org/registry/ingest/manifest/'
+
+    # @return [URI] the profile URI
     attr_reader :profile
 
+    # Creates a new manifest
+    # @param profile [URI, String] the profile URI. Must begin with
+    # @raise [ArgumentError] if `profile` does not begin with {PROFILE_BASE_URI}
+    # @raise [URI::InvalidURIError] if `profile` cannot be parsed as a URI
     def initialize(profile:)
       @profile = valid_profile_uri(profile)
     end
@@ -15,7 +23,7 @@ module Merritt
     def valid_profile_uri(profile)
       profile_uri = to_uri(profile)
       raise ArgumentError, "Invalid profile: #{profile || 'nil'}" unless profile_uri &&
-        profile_uri.to_s.start_with?('http://uc3.cdlib.org/registry/ingest/manifest/')
+        profile_uri.to_s.start_with?(PROFILE_BASE_URI)
       profile_uri
     end
 
